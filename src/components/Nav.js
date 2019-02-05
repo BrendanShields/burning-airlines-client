1b2617cd1174fe7fedcaae3d533418f0325169dc
Brendan Shields <brendan.m.j.shields@gmail.com>
2019-02-05 19:09:58 +1100

    Basic routes created

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>Flights</h2>;
+const Users = () => <h2>User</h2>;
+
+
 class Nav extends Component {
   render () {
     return (
-      <h1> Nav Bar </h1>
-    )
-  }
-}
 
-export default Nav
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about/">About</Link>
+                </li>
+                <li>
+                  <Link to="/users/">Users</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <Route path="/" exact component={Index} />
+            <Route path="/about/" component={About} />
+            <Route path="/users/" component={Users} />
+          </div>
+        </Router>
+
+      );
+    }
+  }
+
+      export default Nav;
