f997d498991a5c2be40a5a58f0e51502c99ed11a
Brendan Shields <brendan.m.j.shields@gmail.com>
2019-02-06 20:56:49 +1100

    Cleaned up the SearchFlight component

--- src/components/SearchFlight.js
+++ src/components/SearchFlight.js
@@ -1,62 +1,53 @@
 import React, { Component } from 'react';
-
+/////////////////// IMPORT COMPONENTS //////////////////////////////////////////
 import Search from './Search'
 import List from './List'
-// import SeatsPanel from './SeatsPanel'
+////////////////// AXIOS SET UP ////////////////////////////////////////////////
 import axios from 'axios'
-
 const FLIGHTS_URL = 'https://burning-airlines-backend.herokuapp.com/flights.json';
 const SEATS_URL = 'https://burning-airlines-backend.herokuapp.com/seats.json'
 const USERS_URL = 'https://burning-airlines-backend.herokuapp.com/users.json'
-
 //////////////// SEARCH FLIGHT AXIOS REQUEST ///////////////////////////////////
 class SearchFlight extends Component{
-
   constructor () {
     super();
-    //creates an array that the flights will be passed into
+
+    // creates an array that the flights will be passed into
     this.state = {
       flights: [],
       seats: [],
       current_seats: []
     }
-    //function to retrieve the data from the server
+
+    //function to retrieve the JSON data from rails API and process into arrays
     const fetchFlights = () => {
       axios.get(FLIGHTS_URL).then( (results) => {
-
         this.setState({ flights: results.data })
-
       });
     }
     fetchFlights();
 
     const fetchSeats = () => {
       axios.get(SEATS_URL).then( (results) => {
-
         this.setState({ seats: results.data })
-
       });
     }
     fetchSeats();
+
+    // Binding the functions to this
     this._activateLasers = this._activateLasers.bind(this);
     this._handleClick = this._handleClick.bind(this);
     this.saveSeats = this.saveSeats.bind(this);
-
   }
 
-  _handleClick(flight_id){
-    // e.preventDefault();
-    this.saveSeats(flight_id);
+  // Runs the saveSeats function
+  _handleClick(seat_id){
+    this.saveSeats(seat_id);
   };
 
+  // UPDATES SEAT WITH USER_ID /////////////////////////////////////////////////
   saveSeats(seat_id) {
     console.log(seat_id);
-    // .post("/api/Users", {
-    //             userFirstName: this.firstName.value,
-    //             userLastName: this.lastName.value,
-    //             userEmail: this.email.value,
-    //             userPassword: this.password1.value
-    //         })
       axios.put(`https://burning-airlines-backend.herokuapp.com/seats/${seat_id}`,
       {
         id: seat_id,
@@ -65,56 +56,40 @@ class SearchFlight extends Component{
       }
     ).then((results) => {
       console.log(results)
-     // this.setState({secrets: [...this.state.secrets, results.data] })
    });
   };
-      _activateLasers(flight_seats){
 
+  // WHEN USER CLICKS ON A FLIGHT, DISPLAYS ALL SEATS ON FLIGHT ////////////////
+  _activateLasers(flight_seats){
         console.log(flight_seats)
         const current_seats = this.state.seats.filter( function( el, index ) {
                return el.flight_id === flight_seats;
            });
-
-      // this.state.seats.map( (seat) =>{
-      //   if (seat.flight_id == flight_seats) {
-      // this.setState({current_seats: seat})
-      //   }
-      //   })
         console.log("current seats array", current_seats);
-      // const current_seats = this.state.seats.filter(function(){
-      //   return seat.flight_id == flight_seats;
-      // })
         this.setState({ current_seats: current_seats })
         console.log("state", this.state.current_seats)
-      //   console.log(current_seats)
       }
 
-
-
-
+  // RENDERS THE VIEW //////////////////////////////////////////////////////////
   render () {
     return (
       <div>
+        {this.state.flights.map((s) =>
+          <div className="square" key={s.id}  onClick={() =>
+            this._activateLasers(s.id)}>
+              Flight {s.id}  From {s.origin} to {s.destination} on {s.date}
+            </div>
+          )}
+        <hr />
 
-    { this.state.flights.map( (s) =>
-    <div className="square" key={s.id}  onClick={() => this._activateLasers(s.id)}>
-    Flight {s.id}  From {s.origin} to {s.destination} on {s.date}
-    </div> )}
-<hr />
-
-    { this.state.current_seats.map( (s) =>
-    <div className="square" key={s.id} onClick={() => this._handleClick(s.id)}>Seat ID: {s.id} user_id: {s.user_id}</div>
-  )}
+       { this.state.current_seats.map( (s) =>
+          <div className="square" key={s.id} onClick={() =>
+            this._handleClick(s.id)}>Seat ID: {s.id} user_id: {s.user_id}
+          </div>
+        )}
      </div>
-
-   )
+    )
   }
 }
-
-
-////////////////// SEARCH SEAT AXIOS REQUEST ///////////////////////////////////
-
-
-
 
 export default SearchFlight;
